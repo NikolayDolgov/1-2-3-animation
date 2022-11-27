a29fee3982109a0c92b9c47ae2c9bf34248962a0
Nikolay Dolgov <koly201020@yandex.ru>
2022-11-27 20:32:39 +0300

    Fix the error

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,10 @@ function Card() {
 
   return (
     <div className='card'>
-      <div className="card__front rotate">
+      <div className="card__front">
         <span className="card__text">front</span>
       </div>
-      <div className="card__back rotate">
+      <div className="card__back">
         <span className="card__text">back</span>
       </div>
     </div>)
